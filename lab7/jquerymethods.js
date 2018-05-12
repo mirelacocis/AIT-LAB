119b76db50b10b5f7ee090c8c51c07688b0e618a
mirelacocis <39051175+mirelacocis@users.noreply.github.com>
2018-05-12 13:21:46 +0300

    Add files via upload

---
+++ lab7/jquerymethods.js
@@ -0,0 +1,16 @@
+function onclickItem(div){
+
+		$(".maincenter").hide();
+		$(".second").hide();
+		
+		$(div).show();;
+
+}
+
+function showMenu(){
+	$('#submenu').toggle();
+}
+
+function toggler(){
+	$('#togg').toggle();
+}
